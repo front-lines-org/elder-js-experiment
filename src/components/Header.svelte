b23ccf84133e70108013d76ca3ec06ef238bf58e
Ruben Soerdien <ruben.soerdien@hotmail.com>
2022-07-20 20:28:27 +0200

    init

---
+++ src/components/Header.svelte
@@ -0,0 +1,20 @@
+<div>
+    <!--
+      Note: tabs must be unique. (They cannot === each other.)
+    -->
+    <TabBar tabs={['Home', 'Merchandise', 'About Us']} let:tab bind:active>
+      <!-- Note: the `tab` property is required! -->
+      <Tab {tab}>
+        <Label>{tab}</Label>
+      </Tab>
+    </TabBar>
+  
+    <pre class="status">Selected: {active}</pre>
+  </div>
+  
+  <script>
+    import Tab, { Label } from '@smui/tab';
+    import TabBar from '@smui/tab-bar';
+  
+    let active = 'Home';
+  </script>
